1943caa5286a765dfc740c7c939a2ce6ecfe5ebe
nima makhmali <154757931+nimamakhmali@users.noreply.github.com>
2024-03-17 17:45:08 +0330

    class

--- Library.cpp
+++ Library.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
+class Person
+{
+ private: 
+  char nameLibrarian[20];
+  char nameReferred[20];
+ public:
+
+};
+class Book
+{};
+class Computer
+{};
+class List
+{};
+class bookNo
+{
+ private:
+   char bookNumber[100];
+ public:
+
+};
+
+
 int main()
 {
-  
-}
+    
+  return 0;
+}  
