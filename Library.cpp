dc67926d01f603a6d9631575a0749d673c94c288
nima makhmali <154757931+nimamakhmali@users.noreply.github.com>
2024-03-23 19:24:29 +0330

    new edition

--- Library.cpp
+++ Library.cpp
@@ -3,154 +3,173 @@
 #include <ctime>
 #include <fstream>
 #include <Windows.h>
+
 using namespace std;
 
 #define UP 72
 #define DOWN 80
-
-class Account;
+Account account;
 
 class Menu
 {
-   Account account;
-  private:
-    unsigned int marker;        
+    
+private:
+    unsigned int marker;
     unsigned int choice = '1';
-  public:
+  
+public:
     void menu()
     {
-      system("cls");
-      cout<<"\t\t\t\tHELLO"<<endl
-      <<"\n\n\t\t   ((WELCOME TO MAKHMALI LIBRARY))"<<
-      "\n\n\n\t\t\t Press any key...";
-      getch();
-      system("cls");
-      do              
-      {
-          system("cls");
-          cout  << "========= Menu ========="  << endl;   
-          if (choice == 1)                   
-              cout << "->" ;  
-          else
+        system("cls");
+        cout << "\t\t\t\tHELLO" << endl
+             << "\n\n\t\t   ((WELCOME TO MAKHMALI LIBRARY))"
+             << "\n\n\n\t\t\t Press any key...";
+        getch();
+        system("cls");
+        do
+        {
+            system("cls");
+            cout << "========= Menu =========" << endl;
+            if (choice == 1)
+                cout << "->";
+            else
+                cout << "  ";
+            cout << " LOG IN or SIGN UP" << endl;
+            if (choice == 2)
+                cout << "->";
+            else
+                cout << "  ";
+            cout << " SEARCH..." << endl;
+            if (choice == 3)
+                cout << "->";
+            else
+                cout << "  ";
 
-              cout << "  ";
-          cout << " LOG IN or SIGN UP" << endl; 
-          if (choice == 2)                    
-              cout << "->" ;   
-          else
-              cout << "  ";
-          cout << " SEARCH..." << endl;
-          if (choice == 3)                   
-              cout << "->" ;  
-          else
-              cout << "  ";
-              
-          cout << " SETTINGS" << endl;
-          if (choice == 4)                    
-              cout << "->" ;   
-          else
-              cout << "  ";
-          cout << " EXIT" << endl;
-          cout << "========================" << endl;
+            cout << " SETTINGS" << endl;
+            if (choice == 4)
+                cout << "->";
+            else
+                cout << "  ";
+            cout << " EXIT" << endl;
+            cout << "========================" << endl;
 
-          marker = getch();   
+            marker = getch();
 
-          switch (marker) 
-          {
-          case UP:               
-              if (choice <= 1)  
-                  choice = 4;   
-              else
-                  choice--;
-              break;
-          case DOWN:           
-              if (choice >= 4)
-                  choice = 1;
-              else
-                  choice++;    
-              break;
-          case 13:   
-              bool flag = true;
-              switch (choice)
-              {
-              case 1:
-                  system("cls");
-                do{ cout<<"\t\t\t\tMAKHMALI LIBRARY";
-                 cout<<"log in or sign up ? ";
-                 char ch; cin>>ch;
-                 if(ch=='l')
-                 {
-                  account.log_in();
-                 }
-                 if(ch=='s')
-                 {
-                  account.sign_up();
-                 }
-                 if(ch=='x')
-                 {
+            switch (marker)
+            {
+            case 72:
+                if (choice <= 1)
+                    choice = 4;
+                else
+                    choice--;
+                break;
+            case 80:
+                if (choice >= 4)
+                    choice = 1;
+                else
+                    choice++;
+                break;
+            case 13:
+                switch (choice)
+                {
+                case 1:
+                    cout << "\t\t\t\tMAKHMALI LIBRARY" << endl;
+                    cout << "Do you want to create an account or log in? (Enter 's' for sign up or 'l' for log in): ";
+                    char option;
+                    cin >> option;
+                    if (option == 's' || option == 'S')
+                    {  
+						account.sign_up();
+                    }
+                    else if (option == 'l' || option == 'L')
+                    {	
+                        account.log_in();
+                    }
                     break;
-                 }
-                  Sleep(10);
-                 }while(true);
-                 break;
-              case 2:
-                    
-                  
-                  break;
-              case 3:
-                  system("cls");
-                  
-                  break;
-              case 4:
-                  system("cls");     
-                  exit(0);       
-              }
-              break;
-          }
-          
-      } while (true);  
-   }
+                case 2:
+
+                    break;
+                case 3:
+                    system("cls");
+
+                    break;
+                case 4:
+                    system("cls");
+                    exit(0);
+                }
+                break;
+            }
+
+        } while (true);
+    }
 };
+
+
 
 class Account
-{ 
-  private:
+{
+private:
     string name;
-    string bookNo;
-    string bookName;
-  public:
+    string code;
+
+public:
     void log_in()
-     {
-      
-     }
+    {
+        cout << "Enter your name: ";
+        cin >> name;
+        cout << "Enter your student number: ";
+        cin >> code;
+
+        ifstream file("accounts.txt");
+        if (file.is_open())
+        {
+            string storedName, storedCode;
+            bool accountFound = false;
+            while (file >> storedName >> storedCode)
+            {
+                if (storedName == name && storedCode == code)
+                {
+                    accountFound = true;
+                    break;
+                }
+            }
+            file.close();
+            if (accountFound)
+            {
+                cout << "You have successfully logged in!" << endl;
+            }
+            else
+            {
+                cout << "Invalid name or student number. Login failed." << endl;
+            }
+        }
+        else
+        {
+            cout << "Unable to open accounts file." << endl;
+        }
+    }
+
     void sign_up()
     {
-      string code;
+        cout << "Enter your name: ";
+        cin >> name;
+        cout << "Enter your student number: ";
+        cin >> code;
 
-    cout << "Enter your name: ";
-    cin >> name;
-
-    cout << "Enter your student number: ";
-    cin >> code;
-
-    ofstream file("accounts.txt", ios::app);
-    if (file.is_open())
-    {
-        file << name << " " << code << endl;
-        cout << "Account created successfully!" << endl;
-        file.close();
+        ofstream file("accounts.txt", ios::app);
+        if (file.is_open())
+        {
+            file << name << " " << code << endl;
+            cout << "Account created successfully!" << endl;
+            file.close();
+        }
+        else
+        {
+            cout << "Unable to open accounts file." << endl;
+        }
     }
-    else
-    {
-        cout << "Unable to open file." << endl;
-    }
-    }
-
-    void accounts()
-    {
-
-    } 
 };
+
 class Chairman
 {
   private:
@@ -174,22 +193,18 @@ class BookList
         ifstream Booklist("booklist.txt" , ios::in | ios::out );
     }
 };
-class Math_Shelf_Manager
-{};
-class Programming_Shelf_Manager
-{};
-class Physic_Shelf_Manager
-{};
-class English_Shelf_Manager
-{};
+
+class Assistant
+{
+  private:
+  public:
+};
 
 
 int main()
 {
-  Menu main_menu;
-  main_menu.menu();    
-  BookList booklist;
-  Account account;
-  Chairman chairman;
-  return 0;
-}  
+    Account account;
+    Menu main_menu;
+    main_menu.menu();
+    return 0;
+}
